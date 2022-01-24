8c4dbc349ac87fac955fbf9c8d3dcdab47711579
Sebastien Chiasson <sebastien.chiasson@statcan.gc.ca>
2022-01-24 18:34:49 +0000

    Add privacy check

--- sebastien.R
+++ sebastien.R
@@ -6,10 +6,14 @@ library(synthpop)
 # Read dataset, cast sex to factor
 df <- read.csv("./data/satgpa.csv") %>%
   mutate(sex = as.factor(sex)) %>%
-  select(-sat_sum)
+  # select(-sat_sum)
+  select(sex, hs_gpa, fy_gpa, sat_sum, sat_v, sat_m)
 
 # Synthesize
 syn_df <- syn(df)
+
+# Summary
+summary(syn_df)
 
 # Diagnostics
 utility.gen(syn_df, df)
@@ -19,3 +23,6 @@ multi.compare(syn_df, df, var = "sat_m", by = "sex")
 multi.compare(syn_df, df, var = "hs_gpa", by = "sex")
 multi.compare(syn_df, df, var = "fy_gpa", by = "sex")
 multi.compare(syn_df, df, var = "sat_m", by="sex", cont.type = "boxplot")
+
+# Test whether any obs was replicated
+replicated.uniques(syn_df, df)
