5d8ba5468698396d5abe900baad154eee9cbc087
Sebastien Chiasson <sebastien.chiasson@statcan.gc.ca>
2022-01-24 17:02:39 +0000

    Add basic synthpop usage

--- sebastien.R
+++ sebastien.R
@@ -3,4 +3,19 @@
 library(dplyr)
 library(synthpop)
 
-df <- read.csv("./data/satgpa.csv")
+# Read dataset, cast sex to factor
+df <- read.csv("./data/satgpa.csv") %>%
+  mutate(sex = as.factor(sex)) %>%
+  select(-sat_sum)
+
+# Synthesize
+syn_df <- syn(df)
+
+# Diagnostics
+utility.gen(syn_df, df)
+utility.tables(syn_df, df)
+multi.compare(syn_df, df, var = "sat_v", by = "sex")
+multi.compare(syn_df, df, var = "sat_m", by = "sex")
+multi.compare(syn_df, df, var = "hs_gpa", by = "sex")
+multi.compare(syn_df, df, var = "fy_gpa", by = "sex")
+multi.compare(syn_df, df, var = "sat_m", by="sex", cont.type = "boxplot")
